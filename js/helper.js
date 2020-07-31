d65666f26b513aae8ac9fba48ac2dc9b4a959e1c
Ankit-29 <iamankitkumar29@gmail.com>
2020-07-31 20:18:07 +0530

    Code Refactor

---
+++ js/helper.js
@@ -0,0 +1,36 @@
+clearCanvas = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+
+drawScore = (score) => {
+    ctx.font = "30px Comic Sans";
+    ctx.fillStyle = "White";
+    ctx.textAlign = "center";
+    ctx.fillText(`Score: ${score}`, 80, 25);
+}
+
+changeOscillatingDirection = () => {
+    oscillatingForce = -oscillatingForce;
+}
+
+increaseScore = () => {
+    score += POINT;
+}
+
+
+generateHurdle = () => {
+    hurdles.push(new RoundHurdle(parseInt(Math.random() * ROUND_HURDLE_RANDOM_RADIUS, 10) + ROUND_HURDLE_RADIUS_MIN, parseInt(Math.random() * canvas.width), HURDLE_START_POINT));
+}
+
+getDistance = (osc, rh) => {
+    return Math.sqrt(Math.pow(osc.x - rh.x, 2) + Math.pow(osc.y - rh.y, 2));
+}
+
+getStartingHurdles = () => {
+    return [
+        new RoundHurdle(parseInt(Math.random() * ROUND_HURDLE_RANDOM_RADIUS, 10) + ROUND_HURDLE_RADIUS_MIN-10, parseInt(Math.random() * canvas.width), HURDLE_START_POINT),
+        new RoundHurdle(parseInt(Math.random() * ROUND_HURDLE_RANDOM_RADIUS, 10) + ROUND_HURDLE_RADIUS_MIN-10, parseInt(Math.random() * canvas.width), HURDLE_START_POINT + 300),
+        new RoundHurdle(parseInt(Math.random() * ROUND_HURDLE_RANDOM_RADIUS, 10) + ROUND_HURDLE_RADIUS_MIN-10, parseInt(Math.random() * canvas.width), HURDLE_START_POINT + 100),
+    ];
+}
--- js/index.js
+++ js/index.js
@@ -1,82 +1,64 @@
 const canvas = document.querySelector('canvas');
-const sound = document.querySelector('audio');
-
+const ctx = canvas.getContext('2d');
 canvas.width = canvas.closest('div').offsetWidth;
 canvas.height = canvas.closest('div').clientHeight;
 
-const ctx = canvas.getContext('2d');
+
+const sound = document.querySelector('audio');
 
 
 let score = 0;
-let position = canvas.width / 2;
-let force = 5;
+let oscillatingForce = OSC_FORCE;
 let flag = false;
-let hurdles = [
-    new RoundHurdle(parseInt(Math.random() * 50, 10) + 20, parseInt(Math.random() * canvas.width), 20),
-    new RoundHurdle(parseInt(Math.random() * 50, 10) + 20, parseInt(Math.random() * canvas.width), canvas.height / 2),
-    new RoundHurdle(parseInt(Math.random() * 50, 10) + 20, parseInt(Math.random() * canvas.width), canvas.height / 5),
-];
 
-const osc = new Oscillator(15, canvas.width / 2, canvas.height - 200, "#66fcf1");
-generateHurdle = () => {
-    hurdles.push(new RoundHurdle(parseInt(Math.random() * 20, 10) + 50, parseInt(Math.random() * canvas.width), 20));
-}
+let hurdles = getStartingHurdles();
 
-getDistance = (osc, rh) => {
-    return Math.sqrt(Math.pow(osc.x - rh.x, 2) + Math.pow(osc.y - rh.y, 2));
-}
+const osc = new Oscillator(OSC_RADIUS, canvas.width / 2, canvas.height - 200, OSC_COLOR);
 
-drawTest = () => {
+gamePlay = () => {
     sound.play();
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.font = "30px Comic Sans";
-    ctx.fillStyle = "White";
-    ctx.textAlign = "center";
-    ctx.fillText("Score: " + score, 70, 25);
-
+    clearCanvas();
+    drawScore(score);
     osc.draw();
     hurdles.forEach(hurdle => {
         hurdle.draw();
     });
 
-    if (hurdles[0].y > canvas.height) {
-        hurdles.shift();
-        console.log(hurdles);
-        score += 10;
-    }
-
-    if (flag) {
-        hurdles.forEach(hurdle => {
-            hurdle.y += 7;
-        });
-    }
-
     for (let i = 0; i < hurdles.length; i++) {
-        // console.log(getDistance(osc, hurdles[i]) < (osc.radius + hurdles[i].radius));
-        if (getDistance(osc, hurdles[i]) < (osc.radius + hurdles[i].radius) - 3) {
+        if (getDistance(osc, hurdles[i]) < (osc.radius + hurdles[i].radius)) {
             navigator.vibrate([200, 200, 200, 200]);
             setTimeout(() => {
                 alert(`Score ${score}`);
+                window.location.reload();
             })
             sound.pause();
             return;
         }
     }
 
+    if (flag) {
+        hurdles.forEach(hurdle => {
+            hurdle.y += GAME_SPEED;
+        });
+    }
+    
+    if (hurdles[0].y > canvas.height) {
+        hurdles.shift();
+        increaseScore();
+    }
+
+    
     if (hurdles[hurdles.length - 1].y >= canvas.height / 2.5) {
         generateHurdle();
     }
-    // console.log(hurdles[0].y > canvas.height);
 
-    if (osc.x <= 15) {
-        force = 5;
+    if (osc.x >= canvas.width-15 || osc.x <= 15) {
+        changeOscillatingDirection();
     }
-    if (osc.x >= canvas.width-15) {
-        force = -5;
-    }
-    osc.x += force;
+    
+    osc.x += oscillatingForce;
 
-    requestAnimationFrame(() => { drawTest(); });
+    requestAnimationFrame(() => { gamePlay(); });
 
 }
 
@@ -85,6 +67,5 @@ canvas.addEventListener("mouseup", () => { flag = false });
 canvas.addEventListener("touchstart", () => { flag = true });
 canvas.addEventListener("touchend", () => { flag = false });
 
-// generateHurdle();
-drawTest();
+gamePlay();
 
